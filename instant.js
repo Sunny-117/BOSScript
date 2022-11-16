4a9d5b503b2212457b0278b714f81c816ba0a6cb
Jesslynwong <jesslynwjx@gmail.com>
2022-11-16 14:18:02 +0800

    feat: add button for automatically click link

--- instant.js
+++ instant.js
@@ -1,25 +1,3 @@
-// console.log(location.href.includes('job_detail'), 'asmdlka')
-// if (location.href.includes('job_detail')) {
-
-// }
-
-// document.addEventListener("DOMContentLoaded", (e) => {
-//     console.log('DOMContentLoaded')
-//     const btn = document.getElementsByClassName('btn-container')[0]
-
-//     if (btn) {
-//         const aLink = btn.getElementsByClassName('btn-startchat')[0]
-//         if (aLink) {
-//             aLink.click()
-//         } else {
-//             console.log('aLink挂了', aLink)
-//         }
-//     } else {
-//         console.log('btn挂了', btn)
-//     }
-// })
-
-
 
 // js跨浏览器标签页通信 storage
 // iframe
@@ -29,50 +7,83 @@
 
 // 定时器5s轮训一次，一旦有了跳出轮训，没有继续，10次后结束提示错误
 
-(function() {
+(function () {
     'use strict';
+    // 添加按钮
+    var btn = document.createElement("button");
+    btn.innerHTML = '批量投递'
+    btn.style.position = 'fixed'
+    btn.style.top = '45%'
+    btn.style.left = '2%'
+    btn.style.border = '1px solid white'
+    btn.style.background = 'red'
+    btn.style.font = '16px'
+    btn.style.color = 'white'
+    btn.style.padding = '5px'
+    btn.style.borderRadius = '5%'
+    document.body.append(btn)
+    
+    btn.onclick = () => {
+        const ulList = document.getElementsByClassName('job-list-box')[0]
+        const lilists = ulList.getElementsByTagName('li');
 
-function fetch() {
-    const btn = document.getElementsByClassName('btn-container')[0]
-    if (btn) {
-        const aLink = btn.getElementsByClassName('btn-startchat')[0]
-        if (aLink) {
-            aLink.click()
-        } else {
-            console.log('aLink挂了', aLink)
+        const autoClick = (url) => {
+            window.open(url)
         }
-    } else {
-        console.log('btn挂了', btn)
+        const waitAutoClickArr = [];
+        [...lilists].forEach(ele => {
+            const a = ele.getElementsByTagName('a')[0]
+            if (a) {
+                waitAutoClickArr.push(a.href)
+            }
+        })
+    
+        waitAutoClickArr.forEach((ele, index) => {
+            autoClick(ele);
+        })
     }
 
-}
-fetch()
-const timer = setInterval(() => {
-    console.log('重新轮训')
+    function fetch() {
+        const btn = document.getElementsByClassName('btn-container')[0]
+        if (btn) {
+            const aLink = btn.getElementsByClassName('btn-startchat')[0]
+            if (aLink) {
+                aLink.click()
+            } else {
+                console.log('aLink挂了', aLink)
+            }
+        } else {
+            console.log('btn挂了', btn)
+        }
+
+    }
     fetch()
-    const textBtn = document.getElementsByClassName('btn-startchat')[0]
-    const leftTitle = document.getElementsByClassName('left-title')[0]
-    const dialogContainer = document.getElementsByClassName('dialog-container')[0]
-    if (!textBtn && location.pathname === '/web/geek/job') {
-        clearInterval(timer)
-        console.log('是主页，不管他')
-        return;
-    }
-    if(leftTitle){
-        clearInterval(timer)
-        window.close()
-        return;
-    }
-    if (dialogContainer) {
-        clearInterval(timer)
-        window.close();
-        return;
-    }
-    if (textBtn.innerText === '继续沟通') {
-        console.log('玩事了')
-        clearInterval(timer)
-        window.close()
-    }
-}, 1000);
+    const timer = setInterval(() => {
+        console.log('重新轮训')
+        fetch()
+        const textBtn = document.getElementsByClassName('btn-startchat')[0]
+        const leftTitle = document.getElementsByClassName('left-title')[0]
+        const dialogContainer = document.getElementsByClassName('dialog-container')[0]
+        if (!textBtn && location.pathname === '/web/geek/job') {
+            clearInterval(timer)
+            console.log('是主页，不管他')
+            return;
+        }
+        if (leftTitle) {
+            clearInterval(timer)
+            window.close()
+            return;
+        }
+        if (dialogContainer) {
+            clearInterval(timer)
+            window.close();
+            return;
+        }
+        if (textBtn.innerText === '继续沟通') {
+            console.log('玩事了')
+            clearInterval(timer)
+            window.close()
+        }
+    }, 1000);
 
 })();
